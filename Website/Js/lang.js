3b56fadf5829eb1e83e305b76c7121e16975c52b
Severin-Nitsche <47613611+Severin-Nitsche@users.noreply.github.com>
2019-07-08 21:44:09 +0200

    Update lang.js

--- Website/Js/lang.js
+++ Website/Js/lang.js
@@ -1,6 +1,10 @@
+var acceptedLangs = ['de'];
+
 var lang = navigator.language || navigator.userlanguage;
 lang = lang.substring(0,2);
-setlang(lang);
+if(acceptedLangs.indexOf(lang)!=-1) {
+  setlang(lang);
+}
 
 async function getText(url) {
   site = await fetch(url);
@@ -12,7 +16,6 @@ async function setlang(lang) {
   current = document.location.pathname.substring(document.location.pathname.lastIndexOf('/')+1,document.location.pathname.lastIndexOf('.'));
   blob = await getText(current+"-"+lang+".lang");
   single = blob.split("\n");
-  p = document.getElementsByTagName('p');
   if(p.length != single.length) return;
   for(let i=0; i<p.length; i++) {
     p[i].innerHTML = single[i];
