32b97950ff55372d52f1d6ed6112bf12aa28df57
Severin-Nitsche <47613611+Severin-Nitsche@users.noreply.github.com>
2019-07-08 20:34:31 +0200

    Update master.js

--- Website/Js/master.js
+++ Website/Js/master.js
@@ -10,3 +10,18 @@ document.addEventListener('DOMContentLoaded',
     }
   }
 )
+
+async function getText(url) {
+  site = await fetch(url);
+  text = await site.text();
+  return text;
+}
+
+async function setlang(lang) {
+  blob = await getText(lang+".lang");
+  single = blob.split("\n");
+  p = document.getElementsByTagName('p');
+  for(let i=0; i<p.length; i++) {
+    p[i].textContent = single[i];
+  }
+}
