56cab501e1dc8fab45dbc2e101d6eb2142a66d29
Severin <severinnitsche@gmail.com>
2019-10-21 11:45:10 +0200

    Refactored

--- Ray Casting/src/Main.java
+++ Ray Casting/src/Main.java
@@ -1,72 +1,50 @@
-import javax.swing.*;
-import java.awt.*;
-import java.awt.image.*;
-import java.awt.event.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.image.BufferedImage;
 import java.awt.Point;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
 public class Main {
-
-//inspect 420 | 266
-
   public static void main(String[] args) {
-    Color       red              = new Color       ( (byte) 255, (byte) 0, (byte) 98 );
-    Color       green            = new Color       ( (byte) 149, (byte) 255, (byte) 0 );
-    Color       blue             = new Color       ( (byte) 10, (byte) 100, (byte) 255 );
-    //ThreeDPlane screen           = new ThreeDPlane (          0,        0,        1,          0,      0,          red,          0 );
-    Sphere      sphere           = new Sphere      (         .5,        1,        3,         1,     red,           .1, true );
-    Sphere      sphere2          = new Sphere      (          0,        2,        2,         .5,  green,           .8, true );
-    ThreeDPlane bottom           = new ThreeDPlane (          0,       0,        0,    Math.PI/2,Math.PI/2,         blue,               0, true);
-    Light       light            = new Light() {};
-    double[] d = {0,10,-15};
+    Color red = new Color((byte)255, (byte)0, (byte)98);
+    Color green = new Color((byte)149, (byte)255, (byte)0);
+    Color blue = new Color((byte)10, (byte)100, (byte)255);
+
+    Sphere sphere = new Sphere(.5, 1, 3, 1, red, .1, true);
+    Sphere sphere2 = new Sphere(0, 2, 2, .5, green, .8, true);
+    ThreeDPlane bottom = new ThreeDPlane(0, 0, 0, Math.PI/2, Math.PI/2, blue, 0, true);
+    Light light = new Light() {};
+    double[] d = {0, 10, -15};
     light.position = d;
     ThreeDObject[] o = {bottom, sphere, sphere2};
     light.objects = o;
     Light[] lights = {light};
-    //System.exit(0);
-    //Ray         ray              = new Ray         (          0,        0,        0,          0,      0,       screen, 2,     sphere2 );
-    //Viewer      viewer           = new Viewer      (          3,        3,      100,     screen, sphere );
-    //Screen      testScreen       = new Screen      (          0,        0,        1,          0,      0, Math.PI / 2d,          0 );
-    //Viewer      testViewer       = new Viewer      (          3,        3,      100, testScreen, sphere );
-    //Viewer      secondTestViewer = new Viewer      (          0,        0,        0,          3,      3,          100, testScreen, sphere );
-    double[]    frontDirection   = {            0, 0 };
-    double[]    leftDirection    = { Math.PI / 2d, 0 };
-    Viewer      thirdTestViewer  = new Viewer      (          0,          3,        -5,          3,      3,         200, frontDirection, leftDirection, 1, 70, lights, bottom, sphere2, sphere );
 
+    double[] frontDirection = {0, 0};
+    double[] leftDirection = {Math.PI / 2d, 0};
+    Viewer thirdTestViewer = new Viewer( 0, 3, -5, 3, 3, 200, frontDirection, leftDirection, 1, 70, lights, bottom, sphere2, sphere);
 
-    //viewer.printColor();
-    //testViewer.printColor();
-    //secondTestViewer.printColor();
-    //thirdTestViewer.printColor();
-
-    JFrame        frame  = new JFrame( "Ray Casting" );
-    BufferedImage image  = new BufferedImage( 600, 600, BufferedImage.TYPE_3BYTE_BGR );
-    image.setRGB( 0, 0, 600, 600, thirdTestViewer.getColorArray(), 0, 600 );
-    frame.add( new JLabel( new ImageIcon( image ) ) );
-    frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
-    frame.setVisible( true );
-    frame.setSize( 600, 610 );
-    frame.addMouseListener(new MouseListener() {
-      @Override
-      public void mouseEntered(MouseEvent e) {
-
+    JFrame frame = new JFrame("Ray Casting");
+    BufferedImage image = new BufferedImage(600, 600, BufferedImage.TYPE_3BYTE_BGR);
+    image.setRGB(0, 0, 600, 600, thirdTestViewer.getColorArray(), 0, 600);
+    frame.add(new JLabel(new ImageIcon(image)));
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setVisible(true);
+    frame.setSize(600, 610);
+    frame.addMouseListener(
+      new MouseListener() {
+        @Override public void mouseEntered(MouseEvent e) {}
+        @Override public void mouseExited(MouseEvent e) {}
+        @Override public void mousePressed(MouseEvent e) {}
+        @Override public void mouseReleased(MouseEvent e) {}
+        @Override public void mouseClicked(MouseEvent e) {
+          thirdTestViewer.inspect((int)e.getPoint().getX(), (int)e.getPoint().getY());
+        }
       }
-      @Override
-      public void mouseExited(MouseEvent e) {
-
-      }
-      @Override
-      public void mousePressed(MouseEvent e) {
-
-      }
-      @Override
-      public void mouseReleased(MouseEvent e) {
-
-      }
-      @Override
-      public void mouseClicked(MouseEvent e) {
-        thirdTestViewer.inspect((int)e.getPoint().getX(),(int)e.getPoint().getY());
-      }
-    });
-
+    );
   }
-
 }
--- Ray Casting/src/Util.java
+++ Ray Casting/src/Util.java
@@ -1,170 +1,171 @@
 public class Util {
 
+  public static final int X = 0;
+  public static final int Y = 1;
+  public static final int Z = 2;
 
-      public static final int X = 0;
-      public static final int Y = 1;
-      public static final int Z = 2;
+  /**
+  *
+  * @param a -&gt; A vector, b -&gt; A vector
+  * @return the dot product
+  *
+  * <p class="warning">!!!IMPORTANT!!! a and b have to have equal lengths</p>
+  *
+  */
+  public static double dot(double[] a, double[] b) {
+    if(a.length != b.length) throw new IllegalArgumentException("Vectors have to be of equal dimensions");
+    double ret = 0;
+    for(int i=0; i<a.length; i++) {
+      ret += a[i] * b[i];
+    }
+    return ret;
+  }
 
-      /**
-      *
-      * @param a -&gt; A vector, b -&gt; A vector
-      * @return the dot product
-      *
-      * <p class="warning">!!!IMPORTANT!!! a and b have to have equal lengths</p>
-      *
-      */
-      public static double dot(double[] a, double[] b) {
-        if (a.length != b.length) throw new IllegalArgumentException("Vectors have to be of equal length");
-        double ret = 0;
-        for(int i=0; i<a.length; i++) {
-          ret += a[ i ] * b[ i ];
-        }
-        return ret;
-      }
+  /**
+  *
+  * <p>converts from spherical to cartesian</p>
+  * <p>ifthe length of p is 2 the value 1 is assumed forr</p>
+  * <p>The general form of p should be {theta, phi, r}</p>
+  *
+  */
+  public static double[] toCartesian(double[] p) {
+    if(p.length > 3 || p.length < 2) throw new IllegalArgumentException("Converting only spherical coordinates");
+    double theta = p[0];
+    double phi = p[1];
+    double r = 1;
+    if(p.length == 3) r = p[2];
+    double x = r * Math.sin(theta) * Math.cos(phi);
+    double y = r * Math.sin(theta) * Math.sin(phi);
+    double z = r * Math.cos(theta);
+    double[] ret = {x, y, z};
+    return ret;
+  }
 
-      /**
-      *
-      * <p>converts from spherical to cartesian</p>
-      * <p>if the length of p is 2 the value 1 is assumed for r</p>
-      * <p>The general form of p should be {theta, phi, r}</p>
-      *
-      */
-      public static double[] toCartesian(double[] p) {
-        if(p.length > 3 || p.length < 2) throw new IllegalArgumentException("Converting only spherical coordinates");
-        double theta = p[ 0 ];
-        double phi   = p[ 1 ];
-        double r     = 1;
-        if(p.length == 3) r = p[ 2 ];
-        double x     = r * Math.sin(theta) * Math.cos(phi);
-        double y     = r * Math.sin(theta) * Math.sin(phi);
-        double z     = r * Math.cos(theta);
-        double[] ret = { x, y, z };
-        return ret;
-      }
+  /**
+  *
+  * @param c - the cartesian coordinates
+  * @return a spherical representation of c
+  *
+  */
+  public static double[] toSpherical(double[] c) {
+    double rho = Math.sqrt(c[X] * c[X] + c[Y] * c[Y] + c[Z] * c[Z]);
+    double phi = Math.atan2(c[Y], c[X]);
+    double theta = Math.acos(c[Z] / rho);
+    double[] returnValue = {theta, phi, rho};
+    return returnValue;
+  }
 
-      /**
-      *
-      * @param c - the cartesian coordinates
-      * @return a spherical representation of c
-      *
-      */
-      public static double[] toSpherical(double[] c) {
-        double   rho         = Math.sqrt( c[ X ] * c[ X ] + c[ Y ] * c[ Y ] + c[ Z ] * c[ Z ] );
-        double   phi         = Math.atan2( c[ Y ], c[ X ] );
-        double   theta       = Math.acos( c[ Z ] / rho );
-        double[] returnValue = { theta, phi, rho };
-        return returnValue;
-      }
+  public static double[] sub(double[] a, double[] b) {
+    if(a.length != b.length) throw new IllegalArgumentException("Vectors have to have equal dimensions");
+    double[] ret = new double[a.length];
+    for(int i=0; i<a.length; i++) {
+      ret[i] = a[i] - b[i];
+    }
+    return ret;
+  }
 
-      public static double[] sub(double[] a, double[] b) {
-        if(a.length != b.length) throw new IllegalArgumentException("Vectors have to have equal length");
-        double[] ret = new double[ a.length ];
-        for (int i=0; i<a.length; i++) {
-          ret[ i ]   = a[ i ] - b[ i ];
-        }
-        return ret;
-      }
+  public static double[] add(double[] a, double[] b) {
+    if(a.length != b.length) throw new IllegalArgumentException("Vectors have to have equal dimensions");
+    double[] ret = new double[a.length];
+    for(int i=0; i<a.length; i++) {
+      ret[i] = a[i] + b[i];
+    }
+    return ret;
+  }
 
-      public static double[] add(double[] a, double[] b) {
-        if(a.length != b.length) throw new IllegalArgumentException("Vectors have to have equal length");
-        double[] ret = new double[ a.length ];
-        for (int i=0; i<a.length; i++) {
-          ret[ i ]   = a[ i ] + b[ i ];
-        }
-        return ret;
-      }
+  public static double[] cross(double[] a, double[] b) {
+    double[] returnValue = {
+                             a[Y] * b[Z] - a[Z] * b[Y],
+                             a[Z] * b[X] - a[X] * b[Z],
+                             a[X] * b[Y] - a[Y] * b[X]
+                           };
+    return returnValue;
+  }
 
-      public static double[] cross(double[] a, double[] b) {
-        double[] returnValue = { a[ Y ] * b[ Z ] - a[ Z ] * b[ Y ],
-                                 a[ Z ] * b[ X ] - a[ X ] * b[ Z ],
-                                 a[ X ] * b[ Y ] - a[ Y ] * b[ X ] };
-        return returnValue;
-      }
+  public static double[] multiply(double[] a, double b) {
+    double[] returnValue = a.clone();
+    for(int index = 0; index < a.length; index++) {
+      returnValue[index] *= b;
+    }
+    return returnValue;
+  }
 
-      public static double[] multiply(double[] a, double b) {
-        double[] returnValue = a.clone();
-        for (int index = 0; index < a.length; index++ ) {
-          returnValue[ index ] *= b;
-        }
-        return returnValue;
-      }
+  public static double[] divide(double[] a, double b) {
+    double[] returnValue = a.clone();
+    for(int index = 0; index < a.length; index++) {
+      returnValue[index] /= b;
+    }
+    return returnValue;
+  }
 
-      public static double[] divide(double[] a, double b) {
-        double[] returnValue = a.clone();
-        for (int index = 0; index < a.length; index++ ) {
-          returnValue[ index ] /= b;
-        }
-        return returnValue;
-      }
+  /**
+  *
+  * @return the projection of a onto b
+  *
+  */
+  public static double[] project(double[] a, double[] b) {
+    double[] projection = multiply(b, Util.dot(a, b) / squaredMagnitude(b));
+    return projection;
+  }
 
-      /**
-      *
-      * @return the projection of a onto b
-      *
-      */
-      public static double[] project(double[] a, double[] b) {
-        double[] projection = multiply( b, Util.dot( a, b ) / squaredMagnitude( b ) );
-        return projection;
-      }
+  /**
+  *
+  * @return the rejection from a on b
+  *
+  */
+  public static double[] reject(double[] a, double[] b) {
+    double[] rejection = sub(a, project(a, b));
+    return rejection;
+  }
 
-      /**
-      *
-      * @return the rejection from a on b
-      *
-      */
-      public static double[] reject(double[] a, double[] b) {
-        double[] rejection = sub( a, project( a, b ) );
-        return rejection;
-      }
+  public static double squaredMagnitude(double[] a) {
+    double result = 0;
+    for(int i = 0; i < a.length; i++) {
+      result += a[i] * a[i];
+    }
+    return result;
+  }
 
-      public static double squaredMagnitude(double[] a) {
-        double result = 0;
-        for( int i = 0; i < a.length; i++ ) {
-          result += a[ i ] * a[ i ];
-        }
-        return result;
-      }
+  public static double magnitude(double[] a) {
+    double result = 0;
+    for(int i = 0; i < a.length; i++) {
+      result += a[i] * a[i];
+    }
+    result = Math.sqrt(result);
+    return result;
+  }
 
-      public static double magnitude(double[] a) {
-        double result = 0;
-        for( int i = 0; i < a.length; i++ ) {
-          result += a[ i ] * a[ i ];
-        }
-        result = Math.sqrt( result );
-        return result;
-      }
+  public static double[] normalize(double[] a) {
+    return divide(a, magnitude(a));
+  }
 
-      public static double[] normalize(double[] a) {
-        return divide( a, magnitude(a) );
-      }
+  public static double map(double value, double min, double max, double nmin, double nmax) {
+    double elevated = value - min;
+    double elevatedMax = max - min;
+    double p = elevated / elevatedMax;
+    double nelevatedmax = nmax - nmin;
+    double nelevated = nelevatedmax * p;
+    double n = nelevated + nmin;
+    return n;
+  }
 
-      public static double map(double value, double min, double max, double nmin, double nmax) {
-        double elevated = value - min;
-        double elevatedMax = max - min;
-        double p = elevated / elevatedMax;
-        double nelevatedmax = nmax - nmin;
-        double nelevated = nelevatedmax * p;
-        double n = nelevated + nmin;
-        return n;
-      }
+  public static byte min(byte a, byte b) {
+    return a<b?a:b;
+  }
 
-      public static byte min(byte a, byte b) {
-        return a<b?a:b;
-      }
+  public static int min(int a, int b) {
+    return a<b?a:b;
+  }
 
-      public static int min(int a, int b) {
-        return a<b?a:b;
-      }
+  public static double clamp(double value, double min, double max) {
+    if(value<min) return min;
+    if(value>max) return max;
+    return value;
+  }
 
-      public static double clamp(double value, double min, double max) {
-        if(value<min) return min;
-        if(value>max) return max;
-        return value;
-      }
-
-      public static String vecToString(double[] a) {
-        return "x:\t"+a[0]+"\r\n"+
-               "y:\t"+a[1]+"\r\n"+
-               "z:\t"+a[2]+"\r\n";
-      }
+  public static String vecToString(double[] a) {
+    return "x:\t"+a[0]+"\r\n"+
+           "y:\t"+a[1]+"\r\n"+
+           "z:\t"+a[2]+"\r\n";
+  }
 }
